ba9e88ae2f4312593f379d3f80dbdf97092dcca7
Noela Andosay <144686117+NMAndosay@users.noreply.github.com>
2024-10-29 02:21:51 +0800

    fixed navbar + added movies page

---
+++ js/navbar.js
@@ -0,0 +1,19 @@
+//Seating Arrangement
+var initializeNavbar = document.querySelector(".NavBarContainer");
+initializeNavbar.innerHTML = `
+<header>
+    <link rel="stylesheet" href="css/navbar.css">
+    <a href="index.html" class="logo">CineBio</a>
+    <ul class="nav">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="movies.html">Movies</a></li>
+        <li><a href="bookings.html">Bookings</a></li>
+        <li><a href="#">Profile</a></li>
+        <li><a href="#">About Us</a></li>
+    </ul>
+    <div class="search">
+        <input type="text" placeholder="Search">
+        <i class="fa fa-search" aria-hidden="true"></i>
+    </div>
+</header>
+`
